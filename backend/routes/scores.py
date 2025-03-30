fcd18deb709f1e3665bab3bb47f866edc4dd6ed3
ramdevv <garvitsharma0108@gmail.com>
2025-03-31 04:12:54 +0530

    fully working code (without security)

--- backend/routes/scores.py
+++ backend/routes/scores.py
@@ -9,32 +9,25 @@ score_bp = Blueprint("score", __name__)
 
 @score_bp.route("/evaluation", methods=["GET"])
 def evaluation():
-    user_id = session.get("user_id")
-
-    if not user_id:
-        return jsonify({"error": "User not logged in"}), 401
-
-    user = user_collection.find_one({"_id": ObjectId(user_id)})
+    user = get_user_by_token(request.cookies.get("login_token"))
 
     if not user:
         return jsonify({"error": "User not found"}), 404
 
-    username = user["username"]
-
     # Fetch all scores for the user
-    user_scores = list(score_collection.find({"username": username}, {"_id": 0}))
+    user_scores = list(score_collection.find({"username": user}, {"_id": 0}))
     print(user_scores)
     apti_score = user_scores[0]["score"]
     comni_score = user_scores[0]["score"]
     technical_score = user_scores[0]["score"]
-    total_compitency = (50 * technical_score) + (30 * apti_score) + (20 * comni_score)
+    total_competency = (50 * technical_score) + (30 * apti_score) + (20 * comni_score)
 
-    print(total_compitency)
+    print(total_competency)
 
     if not user_scores:
         return jsonify({"message": "No scores found for this user"}), 404
 
-    return jsonify(total_compitency)
+    return jsonify(total_competency)
 
 
 @score_bp.route("/dashboard", methods=["GET"])
