4f8b7f90d9d07bc1c4084a254580dc370584fa4a
ramdevv <garvitsharma0108@gmail.com>
2025-04-04 13:49:46 +0530

    updated dashboard

--- backend/routes/scores.py
+++ backend/routes/scores.py
@@ -47,22 +47,31 @@ def get_dasboard_data():
     print(user)
     # Fetch the score
     #  using the insert_id
-    user_scores = score_collection.find_one({"username": user})
-    print("user scores: ", user_scores)
+    user_scores_collections = score_collection.find({"username": user})
+    user_scores = list(user_scores_collections)
+
     if not user_scores:
         return jsonify({"message": "No scores found for this user"}), 404
 
-    print("there is nothing in user score")
-    apti_score = user_scores["apti_score"]
-    comni_score = user_scores["comni_score"]
-    technical_score = user_scores["technical_score"]
-    total_competency = (50 * technical_score) + (30 * apti_score) + (20 * comni_score)
+    response_dict = []
+    quiz_no = 0
+    for scores in user_scores:
 
-    table_score = {
-        "quiz1 aptitude score ": apti_score,
-        "quiz2 communication score ": comni_score,
-        "quiz3 technical score ": technical_score,
-        "compitency score ": (total_competency / 10),
-    }
+        apti_score = scores.get("apti_score", 0)
+        comni_score = scores.get("comni_score", 0)
+        technical_score = scores.get("technical_score", 0)
+        total_competency = (
+            (50 * technical_score) + (30 * apti_score) + (20 * comni_score)
+        )
 
-    return jsonify(table_score)
+        table_entry = {
+            "quiz_score": quiz_no,
+            "quiz1 aptitude score ": apti_score,
+            "quiz2 communication score ": comni_score,
+            "quiz3 technical score ": technical_score,
+            "compitency score ": (total_competency / 10),
+        }
+        response_dict.append(table_entry)
+        quiz_no += 1
+
+    return jsonify(response_dict)
